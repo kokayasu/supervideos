63e01017b2a53e2a767deeaa5c62caafcef6b3f4
Koji Okayasu <koji@Kojis-MacBook-Pro.local>
2023-11-17 15:38:32 +0900

    Add localePath to next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
   debug: process.env.NODE_ENV === "development",
   i18n: {
     locales: ["en", "ja"],
     defaultLocale: "en",
   },
+  localePath: path.resolve("./public/locales"),
 };
