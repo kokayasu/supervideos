d58428e3f259c8d50f91eb4d3ef096c14aafdb8b
Koji Okayasu <koji@Kojis-MacBook-Pro.local>
2023-12-04 19:26:51 -0800

    ii

---
+++ middleware.ts
@@ -0,0 +1,53 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+function getLocaleFromPath(url: URL) {
+  const pathSegments = url.pathname.split("/");
+  const localeSegmentIndex = pathSegments.indexOf("ja"); // Change to your locale identifier
+  if (
+    localeSegmentIndex !== -1 &&
+    pathSegments.length > localeSegmentIndex + 1
+  ) {
+    return pathSegments[localeSegmentIndex];
+  }
+  return "en"; // Default to 'en' if no locale is found
+}
+
+export function middleware(request: NextRequest) {
+  const cookie = request.cookies.get("ageConfirmed");
+  const locale = getLocaleFromPath(new URL(request.url));
+  if (!cookie) {
+    let conformationUrl = "";
+    if (locale === "en") {
+      conformationUrl = "/age-conformation";
+    } else {
+      conformationUrl = `/${locale}/age-conformation`;
+    }
+
+    const url = new URL(conformationUrl, request.url);
+    url.searchParams.set("from", request.url);
+    console.log(url.toString());
+    return NextResponse.redirect(url);
+  }
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/categories/:path*",
+    "/search/:path*",
+    "/videos/:path*",
+    "/",
+
+    "/ja/categories/:path*",
+    "/ja/search/:path*",
+    "/ja/videos/:path*",
+    "/ja/",
+  ],
+};
---
+++ pages/age-conformation.tsx
@@ -0,0 +1,113 @@
+import { Container } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Cookies from "js-cookie";
+import { GetStaticPropsContext } from "next";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Head from "next/head";
+import { useParams } from "next/navigation";
+import { useRouter } from "next/router";
+
+import Footer from "@src/Footer";
+import { translate } from "@src/utils";
+
+function LogoOnlyHeader() {
+  return (
+    <AppBar
+      position="static"
+      elevation={0}
+      sx={{
+        backgroundColor: "white",
+        borderBottom: "1px solid #F6C7C7",
+      }}
+    >
+      <Container maxWidth="xl" sx={{ p: 0 }}>
+        <Toolbar variant="dense">
+          <Typography variant="h1" sx={{ mr: 2 }}>
+            SuperVideos
+          </Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const locale = context.locale as string;
+  const translations = await serverSideTranslations(locale, ["common"]);
+  return {
+    props: {
+      ...translations,
+    },
+  };
+}
+
+export default function Home() {
+  const { t } = useTranslation();
+  const router = useRouter();
+
+  const handleYes = () => {
+    Cookies.set("ageConfirmed", "true");
+    router.push(router.query.from!.toString());
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Super Videos</title>
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+      </Head>
+      <LogoOnlyHeader />
+      <Container
+        maxWidth="sm"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "60vh",
+        }}
+      >
+        <Typography
+          variant="h1"
+          sx={{ fontSize: { xs: "2.5rem" }, marginBottom: 8 }}
+        >
+          {translate(t, "AgeConfirmation")}
+        </Typography>
+        <Typography
+          variant="body1"
+          sx={{ fontSize: "1.2rem", fontWeight: "bold", marginBottom: 4 }}
+        >
+          {translate(t, "AreYouLegalAge")}
+        </Typography>
+        <Typography
+          variant="body1"
+          sx={{ fontSize: "1.2rem", marginBottom: 4, textAlign: "center" }}
+        >
+          {translate(t, "AdultContent")}
+        </Typography>
+
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <a href="https://google.com">
+            <Button variant="outlined" sx={{ width: 150, height: 80 }}>
+              {translate(t, "No")}
+            </Button>
+          </a>
+          <Button
+            variant="contained"
+            onClick={() => handleYes()}
+            sx={{ width: 150, height: 80 }}
+          >
+            {translate(t, "Yes")}
+          </Button>
+        </Box>
+      </Container>
+      <Footer />
+    </>
+  );
+}
